a56d0666a7c9d719f0b5ffc02ce1c5b83291775c
Indra Hidayatulloh <45020393+inhidayatulloh@users.noreply.github.com>
2020-04-23 00:18:27 +0700

    Update KonverterSuhu1.java

--- latihan-soal/KonverterSuhu1.java
+++ latihan-soal/KonverterSuhu1.java
@@ -1,3 +1,8 @@
+/* Programmer			: Indra Hidayatulloh
+   Tanggal dibuat		: 01 April 2020
+   Github/Youtube/Twitter/FB/IG	: inhidayatulloh
+*/
+
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
